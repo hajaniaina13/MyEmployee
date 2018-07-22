94655e52654540e7722aaa331520ffdf7dfee20e
ensor mac <ensor_mac@macbook-air-de-ensor-mac.local>
2018-07-22 08:25:45 +0300

    Initial Commit

---
+++ MyStore/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyStore
+//
+//  Created by ensor_mac on 22/07/2018.
+//  Copyright © 2018 ensor_mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MyStore/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MyStore
+//
+//  Created by ensor_mac on 22/07/2018.
+//  Copyright © 2018 ensor_mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
